cc09fa809ccabca6f9e263006d8751d2908244b7
shyinakira <maxi.solts@gmail.com>
2026-05-26 22:45:25 +0500

    feat: cancel order

--- interservice_connection/b2b_http_client/main.py
+++ interservice_connection/b2b_http_client/main.py
@@ -19,5 +19,21 @@ class B2B_client:
         except requests.ConnectionError as e:
             raise e
 
+    def unreserve_skus(self, order):
+        try:
+            items = []
+            for item in order["items"]:
+                items.append({
+                    "sku_id": item["sku_id"],
+                    "quantity": item["quantity"]
+                })
+            response = self.session.post(f"{self.b2b_url}/api/v1/inventory/unreserve", data={
+                "order_id": order["id"],
+                "items": items
+            })
+            return response
+        except requests.ConnectionError as e:
+            raise e
+
 
 b2b_client = B2B_client()
