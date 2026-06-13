5015243645d36414e23d0b66423caaa4cc87e8d6
Laert <nlovnas@mail.ru>
2026-06-13 19:54:15 +0500

    fix: search

--- src/services/catalog/products.py
+++ src/services/catalog/products.py
@@ -7,14 +7,14 @@ from src.services.categories.get import session, B2B_HOST
 SortType = Literal["rating", "price_asc", "price_desc", "popularity", "new", "discount_desc"]
 
 
-def get_catalog_products(limit: int, offset: int, q: str, sort: SortType, filters: dict):
+def get_catalog_products(limit: int, offset: int, search: str, sort: SortType, filters: dict):
     if sort not in {"rating", "price_asc", "price_desc", "popularity", "new", "discount_desc"}:
         raise ValueError(f"Invalid sort parameter: {sort}")
     category_id = filters.get('category_id', [None])[0] or ""
     filters_string = "&".join(make_filters_query_params(filters))
     # TODO: maybe use `params`?
     r = session.get(f"http://{B2B_HOST}/api/v1/public/products?category_id={category_id}&"
-                    f"limit={limit}&offset={offset}&q={q}&sort={sort}&" + filters_string,
+                    f"limit={limit}&offset={offset}&q={search}&sort={sort}&" + filters_string,
                     headers={"X-Service-Key": B2B_SERVICE_KEY})
     return r.json()
 
